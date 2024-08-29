047b7efcdb4505ea0b8d886d9a00d3a4736d6afa
Łukasz Karasiński <lukasz@karasin.ski>
2024-08-29 10:07:38 +0200

    feat: add conform and configure biome

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -30,6 +30,9 @@ with final.pkgs.lib; let
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
+	# lsp
+	conform-nvim	
+	# ^ lsp
     # nvim-cmp (autocompletion) and extensions
     nvim-cmp # https://github.com/hrsh7th/nvim-cmp
     cmp_luasnip # snippets autocompletion extension for nvim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
---
+++ nvim/plugin/conform.lua
@@ -0,0 +1,20 @@
+if vim.g.did_load_conform_plugin then
+  return
+end
+vim.g.did_load_conform_plugin = true
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		typescriptreact = { 'biome-check' },
+		javascriptreact = { 'biome-check' },
+		typescript = { 'biome-check' },
+		javascript = { 'biome-check' },
+		json = { 'biome-check' }
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})

9f845af0ce82233d896f9837a122a98b556c10bd
Łukasz Karasiński <lukasz@karasin.ski>
2024-09-02 13:40:00 +0200

    feat: configure mini.cursorword

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -6,5 +6,6 @@ vim.g.did_load_plugins_plugin = true
 -- many plugins annoyingly require a call to a 'setup' function to be loaded,
 -- even with default configs
 require('mini.surround').setup()
+require('mini.cursorword').setup()
 require("img-clip").setup({})
 require("dressing").setup({})

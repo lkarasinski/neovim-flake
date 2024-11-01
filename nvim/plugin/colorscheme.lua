d291a45e1c608764e90b5c19bc93add98eb58fa4
Łukasz Karasiński <lukasz@karasin.ski>
2024-11-01 13:40:13 +0100

    feat: update colorscheme

--- nvim/plugin/colorscheme.lua
+++ nvim/plugin/colorscheme.lua
@@ -5,26 +5,26 @@ vim.g.did_load_colorscheme_plugin = true
 
 -- Default options:
 require('kanagawa').setup({
-	compile = false,  -- enable compiling the colorscheme
+	compile = false, -- enable compiling the colorscheme
 	undercurl = true, -- enable undercurls
 	commentStyle = { italic = true },
 	functionStyle = {},
 	keywordStyle = { italic = true },
 	statementStyle = { bold = true },
 	typeStyle = {},
-	transparent = false,   -- do not set background color
-	dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+	transparent = false, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 	terminalColors = true, -- define vim.g.terminal_color_{0,17}
-	colors = {             -- add/modify theme and palette colors
+	colors = {          -- add/modify theme and palette colors
 		palette = {},
 		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 	},
 	overrides = function(colors) -- add/modify highlights
 		return {}
 	end,
-	theme = "wave",    -- Load "wave" theme when 'background' option is not set
-	background = {     -- map the value of 'background' option to a theme
-		dark = "wave", -- try "dragon" !
+	theme = "dragon", -- Load "wave" theme when 'background' option is not set
+	background = { -- map the value of 'background' option to a theme
+		dark = "dragon", -- try "dragon" !
 		light = "lotus"
 	},
 })
